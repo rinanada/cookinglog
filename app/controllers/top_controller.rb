a44c839dc408fbb4a46c77ec8f66b56a9e90260d
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-06-02 18:05:13 +0900

    initial commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,22 @@
+class TopController < ApplicationController
+  def index
+  end
+
+  def introduction
+  end
+
+  def sweets
+  end
+
+  def dinner
+  end
+
+  def chinese
+  end
+
+  def contacts
+  end
+
+  def comments
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'top/index'
+
+  root to: 'top#index'
+  # resource :top, only: :index
+  match "introduction", :to => 'top#introduction',via: [:get]
+  match "sweets", :to=> 'top#sweets', via: [:get]
+  match "dinner", :to=> 'top#dinner', via: [:get]
+  match "chinese", :to=> 'top#chinese', via: [:get]
+  match "contacts", :to=> 'top#contacts', via: [:get]
+  match "comments", :to=> 'top#comments', via: [:get]
+end
